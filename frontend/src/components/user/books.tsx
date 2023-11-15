1552c27e7e1363f39eb5fb5558b507817a5b0197
Manisharath1 <131745767+Manisharath1@users.noreply.github.com>
2023-11-15 15:51:56 +0530

    Update books.tsx
    update

--- frontend/src/components/user/books.tsx
+++ frontend/src/components/user/books.tsx
@@ -1,4 +1,5 @@
-import Book from "@/model/Book";
+import React, { useState } from 'react';
+import Book from '@/model/Book';
 
 const BASE_URL = `http://localhost:1205/api/v1`;
 
@@ -7,24 +8,52 @@ interface BooksProps {
 }
 
 const Books: React.FC<BooksProps> = ({ books }) => {
+  const [approvalStatus, setApprovalStatus] = useState<{ [bookName: string]: 'pending' | 'approved' | 'issued' }>({});
+
+  const handleIssue = (bookName: string) => {
+    
+    setApprovalStatus((prevStatus) => ({ ...prevStatus, [bookName]: 'pending' }));
+
+    
+    setTimeout(() => {
+      setApprovalStatus((prevStatus) => ({ ...prevStatus, [bookName]: 'approved' }));
+
+      setTimeout(() => {
+        setApprovalStatus((prevStatus) => ({ ...prevStatus, [bookName]: 'issued' }));
+      }, 1000); 
+    }, 2000); 
+  };
+
   return (
     <>
       {books.length > 0 ? (
-        books.map(({  bookName, description, author, image }) => (
-          <div className="max-w-sm rounded-lg overflow-hidden shadow-xl bg-white">
+        books.map(({ bookName, description, author, image }) => (
+          <div key={bookName} className="max-w-sm rounded-lg overflow-hidden shadow-xl bg-white mb-4">
+            {/* Add 'mb-4' for margin-bottom */}
             <img
               className="w-full"
               src={`${BASE_URL}/${image}`}
-              alt="Sunset in the mountains"
+              alt="Book Cover"
             />
             <div className="px-6 py-4">
               <div className="font-bold text-xl mb-2">{bookName}</div>
               <p className="text-gray-700 text-base">{description}</p>
             </div>
-            <div className="px-6 pt-4 pb-2">
-              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            <div className="px-6 pt-4 pb-2 flex justify-between items-center">
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mb-2">
                 {author}
               </span>
+              {approvalStatus[bookName] === 'issued' ? (
+                <span className="text-green-500">Issued</span>
+              ) : (
+                <button
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+                  onClick={() => handleIssue(bookName)}
+                  disabled={approvalStatus[bookName] === 'pending' || approvalStatus[bookName] === 'approved'}
+                >
+                  {approvalStatus[bookName] === 'pending' ? 'Approval Pending' : 'Issue'}
+                </button>
+              )}
             </div>
           </div>
         ))
